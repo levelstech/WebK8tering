7aa3be3fa08de044182a6b11b63948d1b99cd143
ReadyRectionz <leroyvelzel@gmail.com>
2022-02-08 02:25:50 +0100

    Added components together

--- k8s-services/api/server.cjs
+++ k8s-services/api/server.cjs
@@ -1,7 +1,8 @@
 'use strict';
 
 var express = require('express');
-var mariadb = require('mariadb');            
+var mariadb = require('mariadb');    
+var cors = require('cors');        
 
 const pool = mariadb.createPool({
    host: process.env.MARIADB_SERVICE_PORT_8306_TCP_ADDR,
@@ -11,58 +12,85 @@ const pool = mariadb.createPool({
    port: process.env.MARIADB_SERVICE_SERVICE_PORT
 })
 
-async function initdb() {
+async function initDB() {
    let conn;
    try {
       conn = await pool.getConnection();
-      const rows = await conn.query("SELECT 1 as val");
-      console.log(rows); 
-      const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
-      console.log(res); 
-
+      await conn.query("DROP TABLE IF EXISTS Counts;");
+      await conn.query("CREATE TABLE IF NOT EXISTS Counts (id bigint primary key AUTO_INCREMENT, counter int DEFAULT 0);");
+      await conn.query("INSERT INTO Counts VALUES (NULL, 0);");
    } catch (err) {
       throw err;
    } finally {
       if (conn) return conn.end();
    }
 }
+
+async function fetchCounts() {
+   let counts = -1
+   let conn;
+   try {
+      conn = await pool.getConnection();
+      let result = await conn.query("SELECT id as id, counter as counter FROM Counts;");
+    
+      counts = result[0]["counter"]
+   } catch (err) {
+      throw err;
+   } finally {
+      if (conn) conn.end();
+
+      return Promise.resolve(counts);      
+   }
+}
+
+async function incrementCounts() {
+   let conn;
+   try {
+      conn = await pool.getConnection();
+      await conn.query("UPDATE Counts SET counter = counter+1 WHERE id > 0;");
+   } catch (err) {
+      throw err;
+   } finally {
+      if (conn) return conn.end();
+   }
+}
+async function clearCounts() {
+   let conn;
+   try {
+      conn = await pool.getConnection();
+      await conn.query("UPDATE Counts SET counter = 0 WHERE id > 0;");
+   } catch (err) {
+      throw err;
+   } finally {
+      if (conn) return conn.end();
+   }
+}
+
 var app = express();
 
 app.set("port", process.env.PORT || 5000);
+app.use(cors({
+   origin: '*'
+}));
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
    res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = { "response" : "This is GET method." }
-   console.log(response);
-   res.end(JSON.stringify(response));
+   let count = await fetchCounts();
+   res.end(JSON.stringify({ "counts" : count }));
 })
 
-app.get('/:id', (req, res) => {
+app.post('/', async (req, res) => {
    res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = { "response" : "This is GET method with id=" + req.params.id + "." }
-   console.log(response);
-   res.end(JSON.stringify(response));
+   await incrementCounts();
+   let count = await fetchCounts();
+   res.end(JSON.stringify({ "counts" : count }));
 })
 
-app.post('/', (req, res) => {
+app.delete('/', async (req, res) => {
    res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = { "response" : "This is POST method." }
-   console.log(response);
-   res.end(JSON.stringify(response));
-})
-
-app.put('/', (req, res) => {
-   res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = { "response" : "This is PUT method." }
-   console.log(response);
-   res.end(JSON.stringify(response));
-})
-
-app.delete('/', (req, res) => {
-   res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = { "response" : "This is DELETE method." }
-   console.log(response);
-   res.end(JSON.stringify(response));
+   await clearCounts();
+   let count = await fetchCounts();
+   res.end(JSON.stringify({ "counts" : count }));
 })
 
 var server = app.listen(app.get("port"), function () {
@@ -72,5 +100,5 @@ var server = app.listen(app.get("port"), function () {
 
   console.log("Node.js API app listening at http://%s:%s", host, port)
 
-   asyncFunction();
+  initDB();
 })
